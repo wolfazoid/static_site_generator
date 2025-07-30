bcce1cf060acea63c06ab97817ca35e4f3116f2e
wolfazoid <wolfahoward@gmail.com>
2025-07-30 17:51:38 -0500

    added headings test to unit tests

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -146,6 +146,20 @@ class TestHTMLNode(unittest.TestCase):
         with self.assertRaises(ValueError):
             node.to_html()
 
+    def test_headings(self):
+        node = ParentNode(
+            "h2",
+            [
+                LeafNode("b", "Bold text"),
+                LeafNode(None, "Normal text"),
+                LeafNode("i", "italic text"),
+                LeafNode(None, "Normal text"),
+            ],
+        )
+        self.assertEqual(
+            node.to_html(),
+            "<h2><b>Bold text</b>Normal text<i>italic text</i>Normal text</h2>",
+        )
 
 if __name__ == "__main__":
     unittest.main()
