11d79dffad7f9fd58a2a1549a50e1035e4ac4976
wolfazoid <wolfahoward@gmail.com>
2025-07-30 10:13:16 -0500

    completed initial set up of TextNode constructor, equivalence test, and repr return value. Set up main.py to initialize a TextNode object and return the string representation.

--- src/main.py
+++ src/main.py
@@ -1,1 +1,8 @@
-print('hello world')
+from textnode import TextNode, TextType
+
+def main():
+    node = TextNode('The quick brown fox jumps over the lazy dog', TextType.LINK, 'https://www.feasting.com')
+    print(node)
+
+if __name__ == "__main__":
+    main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,9 +1,23 @@
 from enum import Enum
 
-class TextNode(Enum):
+class TextType(Enum):
     TEXT = "text"
     BOLD = "bold"
     ITALIC = "italic"
     CODE = "code"
     LINK = "link"
     IMAGE = "image"
+
+class TextNode:
+
+    def __init__(self, text, text_type, url):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, text_node_1, text_node_2):
+        if text_node_1 == text_node_2:
+            return True
+    
+    def __repr__(self):
+        return str(f'TextNode({self.text}, {self.text_type.value}, {self.url})')
