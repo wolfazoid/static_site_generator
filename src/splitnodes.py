3da06b3ad1cc556f11f94603df37c9604fb79217
wolfazoid <wolfahoward@gmail.com>
2025-07-30 20:11:10 -0500

    Refined split nodes function to be more robust and handle various edge cases. Expanded test suite in unit tests.

--- src/splitnodes.py
+++ src/splitnodes.py
@@ -3,12 +3,25 @@ from textnode import TextType, TextNode
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     nodes_list = []
     for node in old_nodes:
-        if node.text_type == TextType.TEXT:
+        if node.text_type != TextType.TEXT:
+            nodes_list.append(node)
+        elif delimiter not in node.text:
+                nodes_list.append(node)
+        else:
+            if node.text.count(delimiter) % 2 != 0:
+                raise Exception("Invalid markdown: missing closing delimiter")
             sub_nodes = node.text.split(delimiter)
-            nodes_list.append(TextNode(sub_nodes[0], TextType.TEXT))
-            for i in range(1,len(sub_nodes)):
+            for i, sub in enumerate(sub_nodes):
+                print(f'\ni: {i}\nsub: {sub}')
+            if node.text.find(delimiter) != 0:
+                nodes_list.append(TextNode(sub_nodes[0], TextType.TEXT))
+            if sub_nodes[-1] == '':
+                sub_nodes.pop(-1)
+            for i in range(1, len(sub_nodes)):
                 if i % 2 == 0:
                     nodes_list.append(TextNode(sub_nodes[i], TextType.TEXT))
                 else:
                     nodes_list.append(TextNode(sub_nodes[i], text_type))
+
+
     return nodes_list
--- src/test_splitnodes.py
+++ src/test_splitnodes.py
@@ -13,3 +13,54 @@ class TestSplitNodesDelimiter(unittest.TestCase):
             TextNode("code block", TextType.CODE),
             TextNode(" word", TextType.TEXT),
         ])
+
+    def test_bold_split(self):
+        node = TextNode("This is text with a **bold block** word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+        self.assertEqual(new_nodes, [
+            TextNode("This is text with a ", TextType.TEXT),
+            TextNode("bold block", TextType.BOLD),
+            TextNode(" word", TextType.TEXT),
+        ])
+
+    def test_delimitter_first(self):
+        node = TextNode("**This is text** with a **bold block** word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+        self.assertEqual(new_nodes, [
+            TextNode("This is text", TextType.BOLD),
+            TextNode(" with a ", TextType.TEXT),
+            TextNode("bold block", TextType.BOLD),
+            TextNode(" word", TextType.TEXT),
+        ])
+    
+    def test_uneven_delimitter(self):
+        node = TextNode("**This is text** with a **bold block word", TextType.TEXT)
+        with self.assertRaises(Exception):
+            new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+
+    def test_inline_delimitter(self):
+        node = TextNode("**This is a node _with an inner delimiter_**", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "_", TextType.ITALIC)
+        self.assertEqual(new_nodes, [
+            TextNode("**This is a node ", TextType.TEXT),
+            TextNode("with an inner delimiter", TextType.ITALIC),
+            TextNode("**", TextType.TEXT),
+        ])
+
+    def test_italic_split(self):
+        node = TextNode("This is text with _italic text_", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "_", TextType.ITALIC)
+        self.assertEqual(new_nodes, [
+            TextNode("This is text with ", TextType.TEXT),
+            TextNode("italic text", TextType.ITALIC),
+        ])
+    
+    def test_embedded(self):
+        node = TextNode("This**messed**up", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+        self.assertEqual(new_nodes, [
+            TextNode("This", TextType.TEXT),
+            TextNode("messed", TextType.BOLD),
+            TextNode("up", TextType.TEXT),
+        ])
+    
