6987c0f0d988dd863cbd48233a94dd0ab10debda
wolfazoid <wolfahoward@gmail.com>
2025-08-04 09:56:01 -0500

    added text_to_nodes method to parse full set of text nodes across each Markdown style. Added tests

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -88,3 +88,12 @@ def split_nodes_link(old_nodes):
             nodes_list.append(TextNode(original_text, TextType.TEXT))
 
     return nodes_list
+
+def text_to_textnodes(text):
+    initial_node = [TextNode(text, TextType.TEXT)]
+    nodes_list = split_nodes_delimiter(initial_node,'**',TextType.BOLD)
+    nodes_list = split_nodes_delimiter(nodes_list,'_',TextType.ITALIC)
+    nodes_list = split_nodes_delimiter(nodes_list,'`',TextType.CODE)
+    nodes_list = split_nodes_image(nodes_list)
+    nodes_list = split_nodes_link(nodes_list)
+    return nodes_list
