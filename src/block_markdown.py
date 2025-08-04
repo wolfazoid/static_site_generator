f1d598fd878d51d7de035325d4cae970c73f8af6
wolfazoid <wolfahoward@gmail.com>
2025-08-04 11:47:57 -0500

    moved line split in block_to_block_type method to top of function, removed from individual if statements.

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -11,25 +11,22 @@ class BlockType(Enum):
 
 def block_to_block_type(block):
     block = block.strip()
-    if block.startswith('#'):
+    lines = block.split('\n')
+    if block.startswith(('# ', '## ', '### ', '#### ', '##### ', '###### ')):
         return BlockType.HEADING
     if block.startswith('```') and block.endswith('```'):
         return BlockType.CODE
     if block.startswith('>'):
-        lines = block.split('\n')
         for line in lines:
             if not line.startswith('>'):
                 break
         return BlockType.QUOTE
     if block.startswith('- '):
-        lines = block.split('\n')
         for line in lines:
-            print(f'Line debug: {line}')
             if not line.startswith('- '):
                 return BlockType.PARAGRAPH
         return BlockType.UNORDERED_LIST
     if block.startswith('1. '):
-        lines = block.split('\n')
         line_count = 1
         for line in lines:
             if not line.startswith(f'{line_count}. '):
@@ -49,3 +46,27 @@ def markdown_to_blocks(markdown):
         blocks_list.append(block)
 
     return blocks_list
+
+def markdown_to_html_node(markdown):
+    blocks_list = markdown_to_blocks(markdown)
+    for block in blocks_list:
+        block_type = block_to_block_type(block)
+
+def block_type_to_tag(block_type):
+    # Set tag based on block type
+    pass
+
+def link_to_props(block):
+    # Convert link to props dictionary
+    # Return HTMLNode with props and tag
+    pass
+
+def image_to_props(block):
+    # convert image to props dictionary
+    # Return HTMLNode with props and tag
+    pass
+
+def block_to_html_node(block, block_type):
+    # Identify block type
+    # send appropriate tag to HTML Node based on block type
+    pass
