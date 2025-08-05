4d960be8c788a84087a6b36d78677aef9392d2cd
wolfazoid <wolfahoward@gmail.com>
2025-08-04 22:15:26 -0500

    reconfigured settings to allow site content to render on Github Pages.

--- src/generate_pages.py
+++ src/generate_pages.py
@@ -3,7 +3,7 @@ import os
 from block_markdown import heading_to_html, markdown_to_blocks, markdown_to_html_node
 from htmlnode import ParentNode
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     
     with open(from_path) as f:
@@ -15,6 +15,8 @@ def generate_page(from_path, template_path, dest_path):
 
     template = template.replace("{{ Title }}", title)
     template = template.replace("{{ Content }}", html_content)
+    template = template.replace('href="/', f'href="{basepath}')
+    template = template.replace('src="/', f'src="{basepath}')
 
     dest_dir_path = os.path.dirname(dest_path)
     if dest_dir_path!= "":
@@ -23,15 +25,15 @@ def generate_page(from_path, template_path, dest_path):
         html_file.write(template) 
     
     
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     for fpath in os.listdir(dir_path_content):
         from_path = os.path.join(dir_path_content, fpath)
         dest_path = os.path.join(dest_dir_path, fpath)
         if dest_path.endswith('md'):
             html_path = dest_path.rstrip('.md') + '.html'
-            generate_page(from_path, template_path, html_path)
+            generate_page(from_path, template_path, html_path, basepath)
         else:
-            generate_pages_recursive(from_path, template_path, dest_path)
+            generate_pages_recursive(from_path, template_path, dest_path, basepath)
 
 def extract_title(markdown):
     if "# " not in markdown:
--- src/main.py
+++ src/main.py
@@ -9,11 +9,15 @@ from generate_pages import generate_pages_recursive
 # from inline_markdown import extract_markdown_images, extract_markdown_links, split_nodes_image, split_nodes_link
 
 dir_path_static = './static'
-dir_path_public = './public'
+dir_path_public = './docs'
 dir_path_content = './content'
 template_path = './template.html'
 
 def main():
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+    else:
+        basepath="/"
     print("Cleaning out destination")
     if os.path.exists(dir_path_public):
         rmtree(dir_path_public)
@@ -21,11 +25,12 @@ def main():
     print("Copying source files to destination directory")
     copy_directories(dir_path_static, dir_path_public)
 
-    print("Generating page...")
+    print("Generating pages...")
     generate_pages_recursive(
         dir_path_content,
         template_path,
-        dir_path_public
+        dir_path_public,
+        basepath
     )
 
 if __name__ == "__main__":
