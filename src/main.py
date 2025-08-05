63ec8cf681533053e91d6c3d162d8b6e0cebb2c4
wolfazoid <wolfahoward@gmail.com>
2025-08-04 22:20:33 -0500

    updated basepath details

--- src/main.py
+++ src/main.py
@@ -13,11 +13,10 @@ dir_path_public = './docs'
 dir_path_content = './content'
 template_path = './template.html'
 
-def main():
+def main(basepath="/"):
     if len(sys.argv) > 1:
         basepath = sys.argv[1]
-    else:
-        basepath="/"
+    print(f"basepath: {basepath}")
     print("Cleaning out destination")
     if os.path.exists(dir_path_public):
         rmtree(dir_path_public)
