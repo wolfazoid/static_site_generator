e3620732f7194ad2bfc369ac6ecda067d0c2bf8e
wolfazoid <wolfahoward@gmail.com>
2025-08-04 19:43:33 -0500

    Built method to copy all of the contents of a given source directory to a given destination directory

--- src/main.py
+++ src/main.py
@@ -1,27 +1,49 @@
 from textnode import TextNode, TextType
 from htmlnode import HTMLNode
 from inline_markdown import extract_markdown_images, extract_markdown_links, split_nodes_image, split_nodes_link
+import os
+from shutil import copy, rmtree
+
+def copy_directories(source='static', destination='public'):
+    if not os.path.exists(source):
+        raise Exception('Source path does not exist, cannot copy files')
+    if not os.path.exists(destination):
+        print(f'Destination directory does not exist, creating new directory named "{destination}"')
+        os.mkdir(destination)
+
+    # Clean up destination directory
+    if len(os.listdir(destination)) > 0:
+        for fpath in os.listdir(destination):
+            full_path = f'{destination}/{fpath}'
+            if os.path.isfile(full_path):
+                os.remove(full_path)
+            elif os.path.isdir(full_path):
+                rmtree(full_path)
+    
+    source_files = os.listdir(source)
+
+    for fpath in source_files:
+        src_path = os.path.join(source,fpath)
+        dst_path = f'{destination}/{fpath}'
+        print(f'file path log\nsrc_path: {src_path}\ndst_path: {dst_path}')
+        if os.path.isfile(src_path):
+            copy(src_path, dst_path)
+        if os.path.isdir(src_path):
+            next_src = os.path.join(src_path)
+            copy_directories(src_path, dst_path)
+    # Copy all files, subdirectories, nested files from source dir
+        # Base Case:
+        # Found a file, copy it
+        # If is directory, 
+        #   get all files and folders in directory
+        #   extend the path to any sub folders
+        #   check any subfolders for files
+        # 
+    # paste into destination director
+    pass
 
 def main():
-    node = TextNode('The quick brown fox jumps over the lazy dog', TextType.LINK, 'https://www.feasting.com')
-    print(node)
-
-    html = HTMLNode(props={
-        "href": "https://www.google.com",
-        "target": "_blank",
-    })
-    print(html.props_to_html())
-
-    text = f'''This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)
-    \nThis is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"'''
-
-    print(extract_markdown_images(text))
-    print(extract_markdown_links(text))
-    node = TextNode(
-        "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)",
-        TextType.TEXT,
-    )
-    split_nodes_link([node])
+    copy_directories(source='not_a_place',destination='a_new_dest')
 
 if __name__ == "__main__":
     main()
